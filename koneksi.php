b1085a29885de3d4ae8bf942a2a4a5a99dd31227
Nikko Adrian <nikkoadrian02@gmail.com>
2025-01-14 07:34:06 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'manajement_gudang';
+    $host = 'localhost';
+    $user = 'root';
+    $pass = 'pyoSXbaltviBuQh3';
+    $db = 'manajement_gudang';
 
-$conn = new mysqli($host, $user, $pass, $db);
+    $conn = new mysqli($host, $user, $pass, $db);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
